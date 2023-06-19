f0a612bebaa999b135088a4a87c795c2bfbdc15d
Artefilex <baris.tncdmr@gmail.com>
2023-06-20 00:10:59 +0300

    database baglantısı kuruldu

--- EJS/index.js
+++ EJS/index.js
@@ -4,9 +4,29 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+const mysql = require("mysql2")
+const config = require("./config")
+let connection = mysql.createConnection( config.db
+)
+
+connection.connect((err)=>{
+    if(err){
+       return  console.log(err)
+    }
+    connection.query("select * from blok" , (err, result)=>{
+       console.log(result)
+    })
+  
+        console.log("mysql server baglantısı yapıldı ")
+    
+
+} )
+
+
 const path = require("path");
 const userRoutes = require("./routes/user");
 const adminRoutes = require("./routes/admin");
+
 
 app.use("/libs", express.static(path.join(__dirname, "node_modules")));
 app.use("/static", express.static(path.join(__dirname, "public")));
